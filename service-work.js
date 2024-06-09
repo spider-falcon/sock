2b4411f9a0c23a13e538576ed69bdc351f61d662
spyderfalcon <67455386+spider-falcon@users.noreply.github.com>
2024-06-09 19:35:08 +0530

    index.html

---
+++ service-work.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'REALM 0.1 Alpha';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  'icon',
+  'icon/Realm_transparent2.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
